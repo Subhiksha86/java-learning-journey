8f41dba386eee8a7c225e234602e23150b148dfd
Subhiksha86 <subhiksharadhakrishnan.86@gmail.com>
2026-07-17 18:40:58 +0530

    Add relational operators

---
+++ 02-Operators/RelationalOperators.java
@@ -0,0 +1,33 @@
+public class RelationalOperators 
+{
+
+    public static void main(String[] args) 
+    {
+        int a = 5;
+        int b = 8 ;
+        boolean greater = a > b ;
+
+        System.out.println(greater);
+
+        double cgpa = 9.18;
+        int  year =  2;
+        boolean eligible = year <= cgpa ;
+
+        System.out.println(eligible);
+
+        float subhi = 9.88f;
+        float thoufi  = 9.99f;
+        boolean different = subhi == thoufi;
+
+        System.out.println(!different);
+
+        String x = "Hello";
+        String y = "Hiiii";
+
+        System.out.println( x != y);
+    
+
+        
+    }
+    
+}
